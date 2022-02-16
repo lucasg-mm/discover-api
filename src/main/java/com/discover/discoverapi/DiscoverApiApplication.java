73acb50b7037dda2987488582b09686755488b17
Lucas Miranda <lucasgmm@usp.br>
2022-02-16 09:51:01 -0300

    Initial commit

---
+++ src/main/java/com/discover/discoverapi/DiscoverApiApplication.java
@@ -0,0 +1,13 @@
+package com.discover.discoverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/discover/discoverapi/DiscoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discover.discoverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
