744b5245c38e656ce9a29445671a67f5b6d76c59
Lucas Miranda <lucasgmm@usp.br>
2022-02-18 16:39:56 -0300

    Added findById method to the AlbumService class

--- src/main/java/com/discover/discoverapi/services/ArtistService.java
+++ src/main/java/com/discover/discoverapi/services/ArtistService.java
@@ -1,4 +1,19 @@
 package com.discover.discoverapi.services;
 
+import com.discover.discoverapi.entities.Artist;
+import com.discover.discoverapi.repositories.ArtistRepository;
+import com.discover.discoverapi.services.exceptions.ObjectNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ArtistService {
+    private final ArtistRepository artistRepository;
+
+    // find by id
+    public Artist findById(long id){
+        return artistRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Artist of id " + id + " not found."));
+    }
 }
--- src/test/java/com/discover/discoverapi/services/ArtistServiceTest.java
+++ src/test/java/com/discover/discoverapi/services/ArtistServiceTest.java
@@ -1,10 +1,19 @@
 package com.discover.discoverapi.services;
 
+import com.discover.discoverapi.entities.Artist;
 import com.discover.discoverapi.repositories.ArtistRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.mockito.Mockito.doReturn;
 
 public class ArtistServiceTest {
     @InjectMocks
@@ -18,5 +27,30 @@ public class ArtistServiceTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    @Test
+    @DisplayName("Tests the retrieval of an artist with an id we already know exists.")
+    public void getAlbumByExistingIdTest() {
+        // --- GIVEN ---
 
+        // input for the get by id method
+        final long id = 1L;
+
+        // mocking repository response when searching for this user
+        Artist expectedFoundArtist = new Artist();
+        expectedFoundArtist.setId(id);
+        Optional<Artist> existingArtistOptional = Optional.of(expectedFoundArtist);
+
+        // the repo returns the correct entity
+        doReturn(existingArtistOptional).when(artistRepository).findById(id);
+
+        // --- WHEN ---
+
+        Artist actualFoundArtist = artistService.findById(id);
+
+        // --- THEN ---
+
+        assertThat("The returned album's id is different than the one being searched." +
+                " Even though the one being searched is also stored.",
+                actualFoundArtist, samePropertyValuesAs(expectedFoundArtist));
+    }
 }
