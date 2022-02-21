86466936b9387ae1413240d670236d7f85fbeb06
Lucas Miranda <lucasgmm@usp.br>
2022-02-21 16:26:35 -0300

    Added GET controller for /albums

--- src/main/java/com/discover/discoverapi/controllers/AlbumController.java
+++ src/main/java/com/discover/discoverapi/controllers/AlbumController.java
@@ -1,4 +1,24 @@
 package com.discover.discoverapi.controllers;
 
+import com.discover.discoverapi.entities.Album;
+import com.discover.discoverapi.services.AlbumService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/albums")
+@AllArgsConstructor
 public class AlbumController {
+    private AlbumService albumService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Album>> findAll(){
+        List<Album> allAlbums = albumService.findAll();
+        return ResponseEntity.ok().body(allAlbums);
+    }
 }
