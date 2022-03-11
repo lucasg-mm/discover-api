c8b4fc2c14b0bcafef037f3224d942a21e72510b
Lucas Miranda <lucasgmm@usp.br>
2022-03-11 12:17:03 -0300

    Added schema description to StandardError

--- src/main/java/com/discover/discoverapi/controllers/exceptions/StandardError.java
+++ src/main/java/com/discover/discoverapi/controllers/exceptions/StandardError.java
@@ -1,13 +1,20 @@
 package com.discover.discoverapi.controllers.exceptions;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Represents a standard error.")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class StandardError {
+    @Schema(description = "Error's http status code.")
     private int status;
+
+    @Schema(description = "Error's timestamp (when the error occurred).")
     private long timestamp;
+
+    @Schema(description = "Error's message.")
     private String message;
 }
