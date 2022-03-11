68d1bf2d1d3674d68ed67bebacfc4b4ec94ed2de
Lucas Miranda <lucasgmm@usp.br>
2022-03-11 15:11:41 -0300

    Added params' description to documentation in GenreController

--- src/main/java/com/discover/discoverapi/controllers/GenreController.java
+++ src/main/java/com/discover/discoverapi/controllers/GenreController.java
@@ -7,6 +7,7 @@ import com.discover.discoverapi.entities.Genre;
 import com.discover.discoverapi.entities.Track;
 import com.discover.discoverapi.services.GenreService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -52,7 +53,8 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @GetMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<Genre> findById(@PathVariable long id){
+    public ResponseEntity<Genre> findById(
+            @Parameter(description="The id from the genre to be retrieved.") @PathVariable long id){
         // retrieves the genre and returns it
         Genre foundGenre = genreService.findById(id);
         return  ResponseEntity.ok(foundGenre);
@@ -95,7 +97,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @PutMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<Genre> updateById(@PathVariable long id, @RequestBody Genre genreDataToUpdate){
+    public ResponseEntity<Genre> updateById(
+            @Parameter(description="Id from the genre to be removed.") @PathVariable long id,
+            @RequestBody Genre genreDataToUpdate){
         // updates the genre
         Genre updatedGenre = genreService.update(id, genreDataToUpdate);
 
@@ -115,7 +119,8 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @DeleteMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<Genre> deleteById(@PathVariable long id){
+    public ResponseEntity<Genre> deleteById(
+            @Parameter(description="Id from the genre to be removed.") @PathVariable long id){
         // deletes the genre
         genreService.deleteById(id);
 
@@ -136,7 +141,8 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @GetMapping(value = "{genreId}/albums", produces = "application/json")
-    public ResponseEntity<Set<Album>> findAllAlbumsOfGenre(@PathVariable long genreId){
+    public ResponseEntity<Set<Album>> findAllAlbumsOfGenre(
+            @Parameter(description="Id of the genre that the albums should be from.") @PathVariable long genreId){
         // find the genre's albums
         Set<Album> foundAlbums = genreService.findAllAlbumsOfGenre(genreId);
         return ResponseEntity.ok(foundAlbums);
@@ -154,7 +160,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @PutMapping(value = "{genreId}/genres/{albumId}", produces = "application/json")
-    public ResponseEntity<Album> addAlbumToGenre(@PathVariable long genreId, @PathVariable long albumId){
+    public ResponseEntity<Album> addAlbumToGenre(
+            @Parameter(description="Id of the genre that the album should be added to.") @PathVariable long genreId,
+            @Parameter(description="Id of the album to be added to the genre's list of albums.") @PathVariable long albumId){
         // add the album to the list of the genre's albums
         Album addedAlbum = genreService.addAlbumToGenre(genreId, albumId);
         return ResponseEntity.ok(addedAlbum);
@@ -172,7 +180,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @DeleteMapping(value = "{genreId}/genres/{albumId}", produces = "application/json")
-    public ResponseEntity<Album> removeAlbumFromGenre(@PathVariable long genreId, @PathVariable long albumId){
+    public ResponseEntity<Album> removeAlbumFromGenre(
+            @Parameter(description="Id of the genre that the album should be removed from.") @PathVariable long genreId,
+            @Parameter(description="Id of the album to be removed from the genre's list of albums.") @PathVariable long albumId){
         // remove album from the genre's list of albums
         genreService.deleteAlbumFromGenre(genreId, albumId);
         return ResponseEntity.noContent().build();
@@ -191,7 +201,8 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @GetMapping(value = "{genreId}/artists", produces = "application/json")
-    public ResponseEntity<Set<Artist>> findAllArtistsOfGenre(@PathVariable long genreId){
+    public ResponseEntity<Set<Artist>> findAllArtistsOfGenre(
+            @Parameter(description="Id of the genre that the artists should be from.") @PathVariable long genreId){
         // find the genre's artists
         Set<Artist> foundArtists = genreService.findAllArtistsOfGenre(genreId);
         return ResponseEntity.ok(foundArtists);
@@ -209,7 +220,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @PutMapping(value = "{genreId}/artists/{artistId}", produces = "application/json")
-    public ResponseEntity<Artist> addArtistToGenre(@PathVariable long genreId, @PathVariable long artistId){
+    public ResponseEntity<Artist> addArtistToGenre(
+            @Parameter(description="Id of the genre that the artist should be added to.") @PathVariable long genreId,
+            @Parameter(description="Id of the artist that should be added to the genre's list of artists.") @PathVariable long artistId){
         // add the artist to the list of the genre's artists
         Artist addedArtist = genreService.addArtistToGenre(genreId, artistId);
         return ResponseEntity.ok(addedArtist);
@@ -227,7 +240,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @DeleteMapping(value = "{genreId}/artists/{artistId}", produces = "application/json")
-    public ResponseEntity<Artist> removeArtistFromGenre(@PathVariable long genreId, @PathVariable long artistId){
+    public ResponseEntity<Artist> removeArtistFromGenre(
+            @Parameter(description="Id of the genre that the artist should be removed from.") @PathVariable long genreId,
+            @Parameter(description="Id of the artist that should be removed from the genre's list of artists.") @PathVariable long artistId){
         // remove artist from the genre's list of artists
         genreService.deleteArtistFromGenre(genreId, artistId);
         return ResponseEntity.noContent().build();
@@ -246,7 +261,8 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @GetMapping(value = "{genreId}/tracks", produces = "application/json")
-    public ResponseEntity<Set<Track>> findAllTracksOfGenre(@PathVariable long genreId){
+    public ResponseEntity<Set<Track>> findAllTracksOfGenre(
+            @Parameter(description="Id of the genre that the tracks should be from.") @PathVariable long genreId){
         // find the genre's tracks
         Set<Track> foundTracks = genreService.findAllTracksOfGenre(genreId);
         return ResponseEntity.ok(foundTracks);
@@ -264,7 +280,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @PutMapping(value = "{genreId}/tracks/{trackId}", produces = "application/json")
-    public ResponseEntity<Track> addTrackToGenre(@PathVariable long genreId, @PathVariable long trackId){
+    public ResponseEntity<Track> addTrackToGenre(
+            @Parameter(description="Id of the genre that the track should be added to.")  @PathVariable long genreId,
+            @Parameter(description="Id of the track to be added to the genre's list of tracks.") @PathVariable long trackId){
         // add the track to the list of the genre's tracks
         Track addedTrack = genreService.addTrackToGenre(genreId, trackId);
         return ResponseEntity.ok(addedTrack);
@@ -282,7 +300,9 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @DeleteMapping(value = "{genreId}/tracks/{trackId}", produces = "application/json")
-    public ResponseEntity<Track> removeTrackFromGenre(@PathVariable long genreId, @PathVariable long trackId){
+    public ResponseEntity<Track> removeTrackFromGenre(
+            @Parameter(description="Id of the genre that the track should be removed from.") @PathVariable long genreId,
+            @Parameter(description="Id of the track to be removed from the genre's list of tracks.") @PathVariable long trackId){
         // remove track from the genre's list of tracks
         genreService.deleteTrackFromGenre(genreId, trackId);
         return ResponseEntity.noContent().build();
@@ -299,9 +319,10 @@ public class GenreController {
                     content = @Content(schema = @Schema(implementation = StandardError.class)))
     })
     @GetMapping(value = "/search", produces = "application/json")
-    public ResponseEntity<Map<String, Object>> findByNameContaining(@RequestParam String name,
-                                                                    @RequestParam(defaultValue = "1") int pageNumber,
-                                                                    @RequestParam(defaultValue = "3") int pageSize){
+    public ResponseEntity<Map<String, Object>> findByNameContaining(
+            @Parameter(description="The genre's name that should be searched for.") @RequestParam String name,
+            @Parameter(description="The number of the page that should be retrieved (starting with 1).") @RequestParam(defaultValue = "1") int pageNumber,
+            @Parameter(description="Number of items in each page.") @RequestParam(defaultValue = "3") int pageSize){
         Map<String, Object> response = genreService.findByNameContaining(name, pageNumber, pageSize);
         return ResponseEntity.ok().body(response);
     }
