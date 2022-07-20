3a6147c276cc0c1e81764f5132a970c0ba4ed0bc
Lucas Miranda <lucasgmm@usp.br>
2022-07-20 10:42:19 -0300

    Added wrapper class SecurityAppUser

---
+++ src/main/java/com/discover/discoverapi/entities/SecurityAppUser.java
@@ -0,0 +1,56 @@
+package com.discover.discoverapi.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+// this is a wrapper class, so we can use our own user class (AppUser)
+// in conjunction with Spring Security
+public class SecurityAppUser implements UserDetails {
+    private final AppUser appUser;
+
+    public SecurityAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(appUser.getRole()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
