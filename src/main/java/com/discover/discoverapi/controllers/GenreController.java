68f7dbc2b649f3cda5e1f6b2547d21a298c01d4f
Lucas Miranda <lucasgmm@usp.br>
2022-07-26 16:55:20 -0300

    Added UserController

--- src/main/java/com/discover/discoverapi/controllers/GenreController.java
+++ src/main/java/com/discover/discoverapi/controllers/GenreController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
---
+++ src/main/java/com/discover/discoverapi/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.discover.discoverapi.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@CrossOrigin
+@RestController
+@AllArgsConstructor
+@Tag(name = "User controller. These endpoints are meant to be used in login situations.")
+public class UserController {
+    @Operation(description = "Gets the logged user. It's used for login.")
+    @RequestMapping("/user")
+    public Principal getUser(Principal user){
+        return user;
+    }
+}
