6b8a95a73d31d8938df211447e8a30c9cb84b55f
Lucas Miranda <lucasgmm@usp.br>
2022-07-27 12:27:58 -0300

    Added JWT validation filter class

---
+++ src/main/java/com/discover/discoverapi/constants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.discover.discoverapi.constants;
+
+public class SecurityConstants {
+    public static final String JWT_KEY = "xxgEQ1XduPq8VdbysFNk5NdudQ53YUn4";
+    public static final String JWT_HEADER = "Authorization";
+}
