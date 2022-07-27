e2acd55c5688409b1c69fffe2051ebe967813954
Lucas Miranda <lucasgmm@usp.br>
2022-07-27 12:29:44 -0300

    Changed JWT validator code to correct file

--- src/main/java/com/discover/discoverapi/filters/JWTTokenGeneratorFilter.java
+++ src/main/java/com/discover/discoverapi/filters/JWTTokenGeneratorFilter.java
@@ -19,44 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
-    // validation should not happen when reaching the 'user' route
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return request.getServletPath().equals("/user");
-    }
+public class JWTTokenGeneratorFilter{
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // gets the JWT token (the one that should be validated) from the request header
-        String jwt = request.getHeader(SecurityConstants.JWT_HEADER);
-
-        if (jwt != null) {
-            try {
-                // gets the secret key from the security constants file
-                SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
-
-                // gets the claims from the received JWT token
-                Claims claims = Jwts.parserBuilder()
-                        .setSigningKey(key)
-                        .build()
-                        .parseClaimsJws(jwt)
-                        .getBody();
-
-                // gets the username from the claim set
-                String username = String.valueOf(claims.get("username"));
-
-                // gets the authorities from the claim set
-                String authorities = (String) claims.get("authorities");
-
-
-                Authentication auth = new UsernamePasswordAuthenticationToken(username,null,
-                        AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            } catch (Exception e) {
-                throw new BadCredentialsException("Invalid token received!");
-            }
-        }
-        filterChain.doFilter(request, response);
-    }
 }
--- src/main/java/com/discover/discoverapi/filters/JWTTokenValidatorFilter.java
+++ src/main/java/com/discover/discoverapi/filters/JWTTokenValidatorFilter.java
@@ -1,4 +1,62 @@
 package com.discover.discoverapi.filters;
 
-public class JWTTokenValidatorFilter {
+import com.discover.discoverapi.constants.SecurityConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JWTTokenValidatorFilter extends OncePerRequestFilter {
+    // validation should not happen when reaching the 'user' route
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().equals("/user");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // gets the JWT token (the one that should be validated) from the request header
+        String jwt = request.getHeader(SecurityConstants.JWT_HEADER);
+
+        if (jwt != null) {
+            try {
+                // gets the secret key from the security constants file
+                SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
+
+                // gets the claims from the received JWT token
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
+                        .parseClaimsJws(jwt)
+                        .getBody();
+
+                // gets the username from the claim set
+                String username = String.valueOf(claims.get("username"));
+
+                // gets the authorities from the claim set
+                String authorities = (String) claims.get("authorities");
+
+
+                Authentication auth = new UsernamePasswordAuthenticationToken(username,null,
+                        AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (Exception e) {
+                throw new BadCredentialsException("Invalid token received!");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
