273a4ce78250d5585fdf8566b3eaaed0212cd8d0
Lucas Miranda <lucasgmm@usp.br>
2022-08-08 15:26:16 -0300

    Added properties to hold a user's lists

--- src/main/java/com/discover/discoverapi/entities/AppUser.java
+++ src/main/java/com/discover/discoverapi/entities/AppUser.java
@@ -1,5 +1,6 @@
 package com.discover.discoverapi.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -8,8 +9,10 @@ import lombok.Setter;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
 
 import javax.validation.constraints.NotEmpty;
+import java.util.Set;
 
 @Schema(description = "Represents a user")
 @Node(labels = {"AppUser"})
@@ -32,6 +35,26 @@ public class AppUser {
 
     @Schema(description = "The user's role in the app, for authorization purposes")
     private String role;
+
+    @Schema(description = "The list of albums liked by the user")
+    @JsonIgnore
+    @Relationship(type = "LIKES", direction = Relationship.Direction.OUTGOING)
+    private Set<Album> likedAlbums;
+
+    @Schema(description = "The list of tracks liked by the user")
+    @JsonIgnore
+    @Relationship(type = "LIKES", direction = Relationship.Direction.OUTGOING)
+    private Set<Track> likedTracks;
+
+    @Schema(description = "The list of genres liked by the user")
+    @JsonIgnore
+    @Relationship(type = "LIKES", direction = Relationship.Direction.OUTGOING)
+    private Set<Genre> likedGenres;
+
+    @Schema(description = "The list of artists liked by the user")
+    @JsonIgnore
+    @Relationship(type = "LIKES", direction = Relationship.Direction.OUTGOING)
+    private Set<Artist> likedArtists;
 
     // CONSTRUCTORS
     public AppUser(String username, String password, String role) {
